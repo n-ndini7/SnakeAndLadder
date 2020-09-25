ba6e9deb1a646caa9d0e28b42929b9f6b33bd637
n-ndini7 <hp@RJs-PC>
2020-09-25 11:28:08 +0530

    UC1 added - starting position of player

--- src/com/capgemini/SnakeAndLadders.java
+++ src/com/capgemini/SnakeAndLadders.java
@@ -1,9 +1,11 @@
 package com.capgemini;
 
 public class SnakeAndLadders {
-	
+	//UC1 Snake and Ladder game played with single player at start position 0
+	public static final int initial_pos =0;
+	public static final int players =1;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snakes and Ladders Game!!!");
-		
+		System.out.println("Player starts at Initial position 0");
 	}
 }
