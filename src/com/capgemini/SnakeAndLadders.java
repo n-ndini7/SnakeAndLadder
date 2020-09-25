6fafb3ffef8e718844e45bf67e79e7cf53093390
n-ndini7 <hp@RJs-PC>
2020-09-25 12:47:50 +0530

    UC7 added - game between two players

--- src/com/capgemini/SnakeAndLadders.java
+++ src/com/capgemini/SnakeAndLadders.java
@@ -1,43 +1,79 @@
 package com.capgemini;
 
 public class SnakeAndLadders {
-	// UC6 The number of times die was rolled to win the game and also the position
-	// of player after very roll.
-	public static final int initial_pos = 0;
-	public static final int players = 1;
-	public static final int no_play = 0;
+	//UC7 - Game bwtween two players:
+	//1.Reporting the position of each player after each die roll.
+	//2.Final position of both when the game ends.
+	//3.Player won and its die roll count required to win the game.
+	public static final int no_play=0;
 	public static final int Ladder = 1;
 	public static final int Snake = 2;
-
+	public static int player1_pos= 0;
+	public static int player2_pos=0;
+	static int p1_turn = 1;  //p1_turn denotes if player 1 turn or not. 
+	static int p2_turn = 2;   //p2_turn denotes if player 2 turn or not.
+	public static int player1_turns = 0;
+	public static int player2_turns = 0;
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Snakes and Ladders Game!!!");
-		System.out.println("Player starts at Initial position 0");
-		int moves = 0;
-		int dice_count = 0;
-		// curr_pos is the current position of player during game
-		int curr_pos = initial_pos;
-		while (curr_pos != 100) {
-			int dice_roll = ((int) Math.floor(Math.random() * 10) % 6) + 1;
-			dice_count++;
-			int check_status = (int) Math.floor(Math.random() * 10) % 3;
-			if (check_status == no_play) {
-				// position of player remains unaltered.
-			} else if (check_status == Ladder) {
-				// current position of player = (current position + dice_roll) for ladder
-				curr_pos += dice_roll;
-				if (curr_pos > 100)
-					curr_pos -= dice_roll;
-			} else {
-				// current position of player = ( current postion dice_roll) for snake
-				curr_pos -= dice_roll;
-
-				if (curr_pos < initial_pos)
-					curr_pos = initial_pos;
+		System.out.println("Player 1 and Player 2 starts at Initial position 0");
+		while((player1_pos<100)&&(player2_pos<100)) {
+		int dice_roll = (int) Math.floor(Math.random()*10)%6 +1;
+		int check_status= (int) Math.floor(Math.random()*10)%3;
+		if(p1_turn==1) {
+			player1_turns++;
+		
+			if(check_status == no_play) {
+				System.out.println("Player 1 remains at same position.");
+				p1_turn = 0;
+				p2_turn = 2;
 			}
-			moves++;
-			System.out.println("Updated Position after every dice roll is : " + curr_pos);
+			else if(check_status== Ladder) {
+				if(player1_pos+dice_roll < 100) {
+					player1_pos+=dice_roll;
+				}
+			}
+			else if(check_status == Snake) {
+				if(player1_pos-dice_roll > 0)
+				 player1_pos-= dice_roll;
+				else
+					player1_pos = 0;
+				p1_turn= 0;
+				p2_turn = 2;
+			}
+			System.out.println("The position of player 1 is: " +player1_pos+ ".");
 		}
-		System.out.println("The Player reaches exactly 100 in " + moves + " moves!");
-		System.out.println("Die has been rolled " + dice_count + " times too reach 100!");
+		else if(p2_turn==2){
+			player2_turns++;
+			if(check_status == no_play) {
+				System.out.println("The Player 2 remains at same position.");
+				p1_turn= 1;
+				p2_turn = 0;
+			}
+			else if(check_status== Ladder) {
+				if(player2_pos+dice_roll <=100)
+					player2_pos+= dice_roll;
+			}
+			else if(check_status == Snake) {
+				if(player2_pos-dice_roll > 0)
+				 player2_pos -= dice_roll;
+				else
+					player2_pos = 0;
+				p1_turn= 1;
+				p2_turn = 0;
+			}
+			System.out.println("The position of player 2 is: "+player2_pos+ ".");
+		}
+		}
+		//final position of both after game ends.
+		System.out.println("The Final Position of player 1 :"+player1_pos+ ".");
+		System.out.println("The Final Position of player 2 :"+player2_pos+ ".");
+		if(player1_pos == 100) {
+			System.out.println("Player 1 won and his dice roll count is :"+player1_turns);
+		}
+		else {
+			System.out.println("Player 2 won and his dice roll count is :"+player2_turns);
+		}
 	}
 }
