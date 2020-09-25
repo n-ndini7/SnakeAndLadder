4f06c407eb43ed717b7bcccb5da73397f6fdc26b
n-ndini7 <hp@RJs-PC>
2020-09-25 11:57:42 +0530

    UC4 added - player reaches 100

--- src/com/capgemini/SnakeAndLadders.java
+++ src/com/capgemini/SnakeAndLadders.java
@@ -1,37 +1,36 @@
 package com.capgemini;
 
 public class SnakeAndLadders {
-	//UC3 The player checks for options - No play , Ladder and Snake.
-	public static final int initial_pos =0;
-	public static final int players =1;
-	public static final int no_play =0;
+	// UC4 repeat till Player reaches 100.
+	public static final int initial_pos = 0;
+	public static final int players = 1;
+	public static final int no_play = 0;
 	public static final int Ladder = 1;
 	public static final int Snake = 2;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snakes and Ladders Game!!!");
 		System.out.println("Player starts at Initial position 0");
-		int dice_roll= ((int) Math.floor(Math.random()*10)%6)+1;
-		System.out.println("Number on the dice rolled: "+dice_roll);
-		//curr_pos is the current position of player during game
-		int curr_pos= initial_pos;
-		int check_status = (int) Math.floor(Math.random()*10)%3;
-		System.out.println("The check status is " + check_status);
-		
-		if(check_status == no_play) {
-			//position of player remains unaltered. 
-			System.out.println("No action taken!");
+		int moves = 0;
+		// curr_pos is the current position of player during game
+		int curr_pos = initial_pos;
+		while (curr_pos < 100) {
+			int dice_roll = ((int) Math.floor(Math.random() * 10) % 6) + 1;
+			int check_status = (int) Math.floor(Math.random() * 10) % 3;
+			if (check_status == no_play) {
+				// position of player remains unaltered.
+			} else if (check_status == Ladder) {
+				// current position of player = (current position + dice_roll) for ladder
+				curr_pos += dice_roll;
+			} else {
+				// current position of player = ( current postion dice_roll) for snake
+				curr_pos -= dice_roll;
+
+				if (curr_pos < initial_pos)
+					curr_pos = initial_pos;
+			}
+			moves++;
 		}
-		else if(check_status == Ladder) {
-			//current position of player = (current position + dice_roll) for ladder
-			curr_pos += dice_roll;
-		}
-		else {
-			//current position of player = ( current postion dice_roll) for snake
-			curr_pos -= dice_roll;
-	
-			if(curr_pos<initial_pos)
-				curr_pos = initial_pos;
-		}
-		System.out.println("The updated position of player after die roll :"+curr_pos);
+		System.out.println("The Player reaches 100 in " + moves + "moves!");
 	}
 }
